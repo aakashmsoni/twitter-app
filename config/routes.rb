fba1703a895fa1619dc70e911e9abe4a5835b927
Aakash Soni <inos92@gmail.com>
2023-03-13 23:26:24 -0500

    added routes for CRUD and set up http client requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # User Index
+  get "/users" => "users#index"
+
+  # User Create
+  post "/users" => "users#create"
+
+  # User Update
+  patch "/users/:id" => "users#update"
+
+  # User Delete
+  delete "/users/:id" => "users#destroy"
+
+  # Tweet Index
+  get "/tweets" => "tweets#index"
+
+  # Tweet Create
+  post "/tweets" => "tweets#create"
+
+  # Tweet Update
+  patch "/tweets/:id" => "tweets#update"
+
+  # Tweet Delete
+  delete "/tweets/:id" => "tweets#destroy"
 end
