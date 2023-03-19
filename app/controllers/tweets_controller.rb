1a2dcb4c5ab95176805a9ef499dc9fda42f5dd69
Aakash Soni <inos92@gmail.com>
2023-03-19 16:04:27 -0500

    updated tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,19 @@
 class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+    render json: @tweets.as_json
+  end
+
+  def update
+    @tweet = Tweet.where(id: params[:id])
+    @tweet.update(
+      user_id: @tweet.user_id,
+      tweet_text: params[:tweet_text] || @tweet.tweet_text,
+    )
+    if @tweet.save
+      render json: @tweet.as_json
+    else
+      render json: { errors: @tweet.errors.full_messages }, status: 422
+    end
+  end
 end
